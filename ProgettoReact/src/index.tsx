9b7ba395bd6ae4fd549aec7c89b30e7de301312a
Cristian-DAttoma <66123531+Cristian-DAttoma@users.noreply.github.com>
2021-03-10 21:08:31 +0100

    LogIn

--- ProgettoReact/src/index.tsx
+++ ProgettoReact/src/index.tsx
@@ -1,20 +1,15 @@
 import { Console } from "console";
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
+import {Header} from './Login';
 import "./styles/app.css";
-import {PostProp} from "./interfaces";
-import {PostListComponent} from "./PostListComponent";
+
 
 
 const App: React.FunctionComponent = () => {
 
     return <>
-            <div className="header">
-                Esercizio Visualizzazione insieme di Post
-            </div>
-            <div className="box">
-                <PostListComponent/>
-            </div>
+            <Header />        
         </>
 }
 
