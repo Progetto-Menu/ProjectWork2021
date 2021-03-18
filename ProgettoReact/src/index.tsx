c0b92e47c224b21462aee7ea07bb8c0090486a29
Cristian-DAttoma <66123531+Cristian-DAttoma@users.noreply.github.com>
2021-03-18 16:56:29 +0100

    FirstPage

--- ProgettoReact/src/index.tsx
+++ ProgettoReact/src/index.tsx
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import {Header} from './Login';
 import "./styles/app.css";
+import {FirstPage} from './firstpage';
 
 
 
 const App: React.FunctionComponent = () => {
 
     return <>
+            <FirstPage />;
             <Header />        
         </>
 }
