a604321f049f0c1484862d34bac900b0a6f1dfa4
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-03-28 18:46:38 +0200

    aggiunta proprietà id_sezione

--- Server/src/model/Piatto.ts
+++ Server/src/model/Piatto.ts
@@ -2,6 +2,7 @@ export class Piatto {
     readonly id: number;
     readonly prezzo: number;
     readonly ordinamento: number;
+    readonly id_sezione: number;
     readonly id_titolo: number;
     readonly id_descrizione: number;
 
@@ -11,14 +12,16 @@ export class Piatto {
     static readonly db_ordinamento = "Ordinamento";
     static readonly db_id_titolo = "IdTitolo";
     static readonly db_id_descrizione = "IdDescrizione";
+    static readonly db_id_sezione = "IdSezione";
 
 
-    constructor(id: number, prezzo: number, ordinamento: number, id_titolo: number, id_descrizione: number) {
+    constructor(id: number, prezzo: number, ordinamento: number, id_titolo: number, id_descrizione: number, id_sezione: number) {
         this.id = id
         this.prezzo = prezzo
         this.ordinamento = ordinamento
         this.id_titolo = id_titolo
         this.id_descrizione = id_descrizione
+        this.id_sezione = id_sezione
     }
 
 }
