bad0e472d71484cd9f3c01defe4243fe9015e8da
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-03-28 18:46:58 +0200

    registrazione

--- ProgettoReact/src/components/Login/RegisterComponent.tsx
+++ ProgettoReact/src/components/Login/RegisterComponent.tsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { Form } from "react-bootstrap";
 import { Users } from "../../utils/Users";
+import axios from "axios";
+import { JSONUtils } from "../../utils/JSONUtils";
+import { TokenUtils } from "../../utils/TokenUtils";
+import { useHistory } from "react-router";
 
 interface RegisterProps {
     user: Users
@@ -9,6 +13,15 @@ interface RegisterProps {
 export const RegisterComponent: React.FunctionComponent<RegisterProps> = (props) => {
 
     const [user, setUser] = useState<Users>(props.user);
+
+    const [nome, setNome] = useState<string>("");
+    const [cognome, setCognome] = useState<string>("");
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [confermaPassword, setConfermaPassword] = useState<string>("");
+    const [accetto, setAccetto] = useState<boolean>(false);
+
+    const history = useHistory();
 
     return <>
         <div className="container mb-3">
@@ -26,42 +39,74 @@ export const RegisterComponent: React.FunctionComponent<RegisterProps> = (props)
                 <div className="col-0 col-md-3" />
                 <Form.Group className="col-12 col-md-6">
                     <Form.Label>Nome</Form.Label>
-                    <Form.Control type="text" placeholder="Nome" />
+                    <Form.Control type="text" placeholder="Nome" value={nome} onChange={(e)=>{
+                        setNome(e.target.value);
+                    }} />
                 </Form.Group>
                 <div className="col-0 col-md-3" />
                 <div className="col-0 col-md-3" />
                 <Form.Group className="col-12 col-md-6">
                     <Form.Label>Cognome</Form.Label>
-                    <Form.Control type="text" placeholder="Cognome" />
+                    <Form.Control type="text" placeholder="Cognome" value={cognome} onChange={(e)=>{
+                        setCognome(e.target.value);
+                    }} />
                 </Form.Group>
                 <div className="col-0 col-md-3" />
                 <div className="col-0 col-md-3" />
                 <Form.Group className="col-12 col-md-6">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Email" />
+                    <Form.Control type="email" placeholder="Email" value={email} onChange={(e)=>{
+                        setEmail(e.target.value);
+                    }} />
                 </Form.Group>
                 <div className="col-0 col-md-3" />
                 <div className="col-0 col-md-3" />
                 <Form.Group className="col-12 col-md-6">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e)=>{
+                        setPassword(e.target.value);
+                    }} />
                 </Form.Group>
                 <div className="col-0 col-md-3" />
                 <div className="col-0 col-md-3" />
                 <Form.Group className="col-12 col-md-6">
                     <Form.Label>Conferma password</Form.Label>
-                    <Form.Control type="password" placeholder="Conferma Password" />
+                    <Form.Control type="password" placeholder="Conferma Password" value={confermaPassword} onChange={(e)=>{
+                        setConfermaPassword(e.target.value);
+                    }} />
                 </Form.Group>
                 <div className="col-0 col-md-3" />
                 <div className="col-0 col-md-3" />
                 <Form.Group className="col-12 col-md-6" controlId="ckbAccetto">
-                    <Form.Check type="checkbox" label="Accetto" />
+                    <Form.Check type="checkbox" label="Accetto" onChange={(e)=>{
+                        setAccetto(e.target.checked)
+                    }} checked={accetto}/>
                 </Form.Group>
                 <div className="col-0 col-md-3" />
                 <div className="col-0 col-md-3" />
                 <div className="col-12 col-md-6 text-right">
                     <button className="btn btn-primary" type="button" onClick={() => {
-
+                        if(user === Users.TRADUTTORE){
+                            axios.post("https://www.progettomenu.cloud/traduttori/register", {
+                                "email": email,
+                                "password": password,
+                                "nome": nome,
+                                "cognome": cognome
+                          }).then(res => {
+                                const token = JSONUtils.getProperty(res.data, "token", null)
+                                if (token != null) {
+                                      TokenUtils.setToken(token);
+                                      history.push("/traduttori/home");
+                                }
+                                else {
+                                      //setErrorMessage("Credenziali Sbagliate")
+                                      //setIsVisibleAlertError(true);
+                                }
+                          }).catch((error) => {
+                                //setErrorMessage("Errore di comunicazione con il server")
+                                //setIsVisibleAlertError(true);
+                          })
+                        }
                     }}> Registrati </button>
                 </div>
                 <div className="col-0 col-md-3" />
