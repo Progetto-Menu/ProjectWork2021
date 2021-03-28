9be0f0bbb6bd27b12f48a7a58be26ce05fa03c20
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-03-28 18:46:03 +0200

    rimossa proprietà nome

--- Server/src/model/Lingua.ts
+++ Server/src/model/Lingua.ts
@@ -1,16 +1,13 @@
 export class Lingua {
     readonly id: number;
-    readonly nome: string;
     readonly cod_lingua: string;
 
     static readonly db_table_name = "lingue";
     static readonly db_id = "Id";
-    static readonly db_nome = "Nome";
     static readonly db_cod_lingua = "Cod_Lingua";
 
-    constructor(id: number, nome: string, cod_lingua: string) {
+    constructor(id: number, cod_lingua: string) {
         this.id = id
-        this.nome = nome
         this.cod_lingua = cod_lingua
     }
 
