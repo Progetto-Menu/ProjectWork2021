ea19654e8d2359d560602973dcbb26c44ba20cc9
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-04-03 17:13:30 +0200

    restyle yourLanguagesComponent

--- ProgettoReact/src/components/Traduttore/Component/yourLanguagesComponent.tsx
+++ ProgettoReact/src/components/Traduttore/Component/yourLanguagesComponent.tsx
@@ -1,11 +1,51 @@
 import React, { Component } from 'react';
+import { Button, Card, Form } from 'react-bootstrap';
+import { Language } from '../Prop/menuProp';
 import { UserProp } from '../Prop/userProp';
 
-export const YourLanguagesComponent: React.FunctionComponent<UserProp> = (prop) => {
+interface YourLanguagesProps{
+    allLanguages: Language[]
+}
+
+export const YourLanguagesComponent: React.FunctionComponent<YourLanguagesProps> = (props) => {
 
     return <React.Fragment>
-        <button className="bg-white w-50 mx-auto rounded border border-secondary text-warning">
-            Your Languages
-        </button>
+        <Card>
+            <Card.Header>
+                Le tue Lingue
+            </Card.Header>
+            <Card.Body>
+                <Form.Group className="col-12">
+                    <Form.Label>Lingue</Form.Label>
+                    <Form.Control as="select">
+                        {props.allLanguages.map((value, index)=>{
+                            return <option key={index} value={value.id}>{value.name}</option>
+                        })}
+                    </Form.Control>
+                </Form.Group>
+                <div className="col-12 text-right mb-3">
+                    <Button variant="success">Aggiungi</Button>
+                </div>
+
+                <div className="table-responsive">
+                    <table className="table table-bordered">
+                        <thead>
+                            <tr><th>Lingue</th>
+                                <th>Codice</th>
+                                <th>Rimuovi</th></tr>
+
+                        </thead>
+                        <tbody>
+                            
+                            <tr>
+                                <td>Italiano</td>
+                                <td>it-IT</td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </Card.Body>
+        </Card>
     </React.Fragment>
 }
