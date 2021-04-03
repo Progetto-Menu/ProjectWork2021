1ab5dcb215bcb0f4b493178d01efe5f612a4e7d0
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-04-03 17:10:44 +0200

    sistemata RouterUtils

--- ProgettoReact/src/utils/RouterUtils.tsx
+++ ProgettoReact/src/utils/RouterUtils.tsx
@@ -64,7 +64,7 @@ export const PrivateRoute = ({ ...routeProps }: PrivateRouteProps) => {
         return <Route {...routeProps} />;
     }
     else {
-        return <></>
+        return <Route {...routeProps} />;
     }
 
 
