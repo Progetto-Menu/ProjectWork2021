f8659f035ecf652d51e47ecba598a381991ed017
Diego Piccinini <diego.piccinini.studio@fitstic-edu.com>
2021-04-11 10:25:48 +0200

    app mobile

--- AppMobile/MenuProject/app/src/main/java/it/fitstic/menuproject/MainActivity.java
+++ AppMobile/MenuProject/app/src/main/java/it/fitstic/menuproject/MainActivity.java
@@ -2,66 +2,87 @@ package it.fitstic.menuproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.net.http.SslError;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 
 
 public class MainActivity extends AppCompatActivity {
 
     WebView webView;
     SwipeRefreshLayout swipe;
+    ImageView noConnection;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        swipe = (SwipeRefreshLayout)findViewById(R.id.swipe);
-        swipe.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        swipe = findViewById(R.id.swipe);
+        webView = findViewById(R.id.webView);
+        noConnection = findViewById(R.id.no_connection);
+        progressBar = findViewById(R.id.progress);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCacheEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.setWebViewClient(new WebViewClient() {
+
             @Override
-            public void onRefresh() {
-                WebAction();
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                noConnection.setVisibility(View.VISIBLE);
+                webView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
             }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                noConnection.setVisibility(View.VISIBLE);
+                webView.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.setVisibility(View.VISIBLE);
+                noConnection.setVisibility(View.GONE);
+                progressBar.setVisibility(View.GONE);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error){
+                handler.proceed();
+            }
+
         });
 
+        swipe.setOnRefreshListener(this::WebAction);
         WebAction();
 
     }
 
 
-    public void WebAction(){
-
-        webView = (WebView) findViewById(R.id.webView);
-        webView.getSettings().setJavaScriptEnabled(true);
-        webView.getSettings().setAppCacheEnabled(true);
-        webView.loadUrl("http://localhost:3000");
-        swipe.setRefreshing(true);
-        webView.setWebViewClient(new WebViewClient(){
-
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-
-                webView.loadUrl("file:///android_assets/error.html");
-
-            }
-
-            public void onPageFinished(WebView view, String url) {
-                // do your stuff here
-                swipe.setRefreshing(false);
-            }
-
-        });
-
+    public void WebAction() {
+        swipe.setRefreshing(false);
+        progressBar.setVisibility(View.VISIBLE);
+        webView.loadUrl("https://www.progettomenu.cloud");
     }
 
 
     @Override
-    public void onBackPressed(){
-
-        if (webView.canGoBack()){
-            webView.goBack();
-        }else {
-            finish();
-        }
+    public void onBackPressed() {
+        if (webView.canGoBack()) webView.goBack();
+        else finish();
     }
 }
